fc16c09a23c6d50eb5771f3f3443b6fcc0c8d109
Alec Wang <44207558+alswang18@users.noreply.github.com>
2021-01-18 19:44:12 -0700

    update settings for fresh installs to work

--- imageRepository/settings.py
+++ imageRepository/settings.py
@@ -4,6 +4,7 @@ import os
 from pathlib import Path
 from dotenv import load_dotenv, find_dotenv
 load_dotenv(find_dotenv())
+from django.core.management.utils import get_random_secret_key
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -12,7 +13,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY')
+SECRET_KEY = os.environ.get('SECRET_KEY', get_random_secret_key())
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = bool(os.environ.get('PRODUCTION'))
